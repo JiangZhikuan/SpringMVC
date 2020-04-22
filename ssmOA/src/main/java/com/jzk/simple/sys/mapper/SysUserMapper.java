6b27ad3d6176280cc29e819b5214e999e9487158
JiangZhikuan <37653872+JiangZhikuan@users.noreply.github.com>
2020-04-22 22:51:18 +0800

    add

---
+++ ssmOA/src/main/java/com/jzk/simple/sys/mapper/SysUserMapper.java
@@ -0,0 +1,35 @@
+package com.jzk.simple.sys.mapper;
+
+import com.jzk.simple.sys.domain.SysUser;
+import com.jzk.simple.sys.domain.SysUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysUserMapper {
+    long countByExample(SysUserExample example);
+
+    int deleteByExample(SysUserExample example);
+
+    int deleteByPrimaryKey(Integer userid);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    List<SysUser> selectByExample(SysUserExample example);
+
+    SysUser selectByPrimaryKey(Integer userid);
+
+    int updateByExampleSelective(@Param("record") SysUser record, @Param("example") SysUserExample example);
+
+    int updateByExample(@Param("record") SysUser record, @Param("example") SysUserExample example);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+
+    /*
+    * 登录
+    * */
+    SysUser login(SysUser user);
+}
